4027b5c8fecb19286f660b806bc64481bd540ca8
Andres Romero <andyasromero@gmail.com>
2022-12-14 22:11:19 -0300

    Creacion preliminar de modelo Posteo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Posteo(models.Model):
+    titulo = models.CharField(max_length= 200, unique= True)
+    # slugField
+    # usuario = 
+    contenido = models.TextField()
+    # imagen = 
+    creacion = models.DateTimeField(auto_now_add= True)
+    modificacion = models.DateTimeField(auto_now= True)
+
+    class Meta:
+        ordering = ['-creacion']    # Para ordenar los post por su fecha de creacion en las querys
+    
+    def __str__(self):
+        return self.titulo
