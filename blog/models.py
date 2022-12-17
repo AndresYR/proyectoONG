18847f95a2256d61a9b7cd77a5ea89d1d0c66cbf
Andres Romero <andyasromero@gmail.com>
2022-12-17 14:14:44 -0300

    Creado superusuario y modif model Posteo

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Posteo(models.Model):
     titulo = models.CharField(max_length= 200, unique= True)
-    # slugField
-    # usuario = 
-    contenido = models.TextField()
+    # slug = models.SlugField(max_length=200, unique= True)
+    # autor = models.ForeignKey(Usuario, on_delete= models.CASCADE, related_name= 'blog_posts')
+    descripcion = models.CharField(max_length=400, null= True)
+    contenido = models.TextField(null= True)
     # imagen = 
     creacion = models.DateTimeField(auto_now_add= True)
     modificacion = models.DateTimeField(auto_now= True)
