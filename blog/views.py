d95c4a1a3557c5f9728f1ca4fa3ddf6c47d79583
Andres Romero <andyasromero@gmail.com>
2022-12-17 18:29:41 -0300

    Agregada una view "posts" prelimitar para visualizar los posteos

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Posteo
+
+def index(request):
+    return render(request, 'blog/index.html')
+
+
+def posts(request):
+    latest_post_list = Posteo.objects.all
+    return render(request, "blog/posts.html", {
+        'latest_post_list': latest_post_list
+    })
